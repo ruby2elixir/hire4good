5da87967784aa3f99a217a7ae6352e0570f96626
Roman Heinrich <roman.heinrich@gmail.com>
2016-08-27 22:30:16 +0200

    analyser has first ouput

--- lib/hire4good/analyzer.ex
+++ lib/hire4good/analyzer.ex
@@ -1,20 +1,38 @@
 defmodule Hire4Good.Analyzer do
-  def analyse(name, projects) do
+  alias Hire4Good.LibrariesIO
+  def analyse(name, type) when type in ["contributions", "repositories"] do
+    projects = LibrariesIO.collect(name, type)
+    analyse(name, projects)
+  end
+
+  def analyse(name, projects) when is_list(projects) do
+    grouped = grouped(projects)
     []
     |> add("*****************************")
-    |> add("Analysis for #{name}")
-    |> add("Number projects: #{length(projects)}")
-    |> add("Languages: #{ inspect grouped(projects)|> Map.keys}")
+    |> add("Analysis for:       #{name}")
+    |> add("Number of projects: #{length(projects)}")
+    |> add("Languages:          #{languages(grouped)}")
+    |> add("Overview")
+    |> add(lang_overview(grouped))
     |> add("*****************************")
     |> Enum.reverse
     |> Enum.join("\n")
     |> IO.puts
   end
 
-  defp add(lines, line), do: [line | lines]
+  defp add(lines, line), do: [line | lines] |> List.flatten
 
   defp grouped(projects) do
     projects
     |> Enum.group_by(fn(x)-> x.lang end)
+  end
+
+  def languages(m), do: (Map.keys(m) -- [nil]) |> Enum.join(", ")
+
+  def lang_overview(grouped) do
+    grouped
+    |> Enum.map(fn({lang, projects})-> [lang, length(projects)] end)
+    |> Enum.sort_by(fn([lang, count])-> count  end)
+    |> Enum.map(fn([l, c])-> "- #{l}: #{c}" end)
   end
 end
