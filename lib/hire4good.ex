9ed4a3a80f371aca31ca696758857e76ac9a29dd
Roman Heinrich <roman.heinrich@gmail.com>
2016-08-27 19:50:39 +0200

    expose functions  on main module

--- lib/hire4good.ex
+++ lib/hire4good.ex
@@ -1,2 +1,9 @@
 defmodule Hire4Good do
+  def contributions(name) do
+    Hire4Good.Contributions.collect(name)
+  end
+
+  def repositories(name) do
+    Hire4Good.Repositories.collect(name)
+  end
 end
